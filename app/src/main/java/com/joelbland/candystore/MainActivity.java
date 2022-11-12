fe7d8815b48202cb5f273b308cea3e88add74858
blandco <jbland@gmail.com>
2022-11-12 06:18:40 -0600

    V1 updates

--- app/src/main/java/com/joelbland/candystore/MainActivity.java
+++ app/src/main/java/com/joelbland/candystore/MainActivity.java
@@ -1,5 +1,6 @@
 package com.joelbland.candystore;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,33 +10,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 //Concept borrowed from Hervé J. Franceschi
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+        Toolbar toolbar = findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public boolean onCreateOptionsMenu( Menu menu ) {
+        getMenuInflater( ).inflate( R.menu.menu_main, menu );
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        int id = item.getItemId( );
         switch ( id ) {
             case R.id.action_add:
                 Log.w( "MainActivity", "Add selected" );
+                Intent insertIntent = new Intent( this, InsertActivity.class );
+                this.startActivity( insertIntent );
                 return true;
             case R.id.action_delete:
                 Log.w( "MainActivity", "Delete selected" );
